d5ba377259277b60471f8b56badb0e82eb36b714
Rohan <rohan.deshpande@bacancy.com>
2022-03-21 02:45:18 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  }
+  
+  const clear = () => {
+    setResult("");
+  }
+
+  const backspace = () => {
+    setResult(result.slice(0, -1));
+  }
+  
+  const calculate = () => {
+    try {
+      setResult(eval(result.toString()));
+    } catch (e) {
+      setResult("Error");
+    }
+  }
+
+  const square = () => {
+    try {
+      var a=eval(result);
+      var temp=a*a;
+      setResult(temp.toString()); 
+    }
+    catch (e) {
+      setResult("Error");
+    }
+  }
+
+  const cube = () => {
+    try {
+      var a=eval(result);
+      var temp=Math.pow(a, 3);;
+      setResult(temp.toString()); 
+    }
+    catch (e) {
+      setResult("Error");
+    }
+  }
+
+  const squareroot = () => {
+    try {
+      var a=eval(result);
+      var temp=Math.sqrt(a);
+      setResult(temp.toString()); 
+    }
+    catch (e) {
+      setResult("Error");
+    }
+  }
+
+  const cubicroot = () => {
+    try {
+      var a=eval(result);
+      var temp=Math.cbrt(a);
+      setResult(temp.toString()); 
+    }
+    catch (e) {
+      setResult("Error");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <form> 
+          <input type='text' value={result}/>
+        </form>
+        <div className='keypad'>
+          <button className='highlight' onClick={clear}>Clr</button>
+          <button className='highlight' onClick={backspace}>C</button>
+          <button className='highlight' name='(' onClick={handleClick}>(</button>
+          <button className='highlight' name=')' onClick={handleClick}>)</button>
+          <button className='highlight' name='/' onClick={handleClick}>&divide;</button>
+
+          <button name='7' onClick={handleClick}>7</button>
+          <button name='8' onClick={handleClick}>8</button>
+          <button name='9' onClick={handleClick}>9</button>
+          <button className='highlight' name='*' onClick={handleClick}>&times;</button>
+          <button className='highlight' name='-' onClick={handleClick}>&ndash;</button>
+          <button name='4' onClick={handleClick}>4</button>
+          <button name='5' onClick={handleClick}>5</button>
+          <button name='6' onClick={handleClick}>6</button>
+          <button className='highlight' name='+' onClick={handleClick}>+</button>
+          <button className='highlight' onClick={square}>sqr</button>
+
+          <button name='1' onClick={handleClick}>1</button>
+          <button name='2' onClick={handleClick}>2</button>
+          <button name='3' onClick={handleClick}>3</button>
+          <button className='highlight' onClick={square}>sqr</button>
+          <button className='highlight' onClick={cube}>cube</button>
+          
+          
+          <button name='0' onClick={handleClick}>0</button>
+          <button name='.' onClick={handleClick}>.</button>
+          <button onClick={calculate} id='result'>=</button>
+          <button className='highlight' onClick={squareroot}>sqrt</button>
+          <button className='highlight' onClick={cubicroot}>cbrt</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
